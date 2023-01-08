f6731506d05827b6e2e0dfe0e25201f22f4ab929
BrillioTushar <119292218+BrillioTushar@users.noreply.github.com>
2023-01-08 22:48:42 +0530

    Add files via upload

---
+++ Backend/src/test/java/com/healthifyme/capstoneJPA/CapstoneJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthifyme.capstoneJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
