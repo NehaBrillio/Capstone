299d9f65375c3e2c33b37692cd92951f25f1cbdd
BrillioTushar <119292218+BrillioTushar@users.noreply.github.com>
2023-01-08 22:29:45 +0530

    Delete capstoneJPA directory

--- capstoneJPA/capstoneJPA/src/main/java/com/healthifyme/capstoneJPA/dao/customerDataRepo.java
+++
@@ -1,9 +0,0 @@
-package com.healthifyme.capstoneJPA.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.healthifyme.capstoneJPA.model.Customer;
-
-public interface customerDataRepo extends JpaRepository<Customer, Integer> {
-
-}
